38833a879c27ba2d0e1d5d29cf69c42305195e5b
Jeff.zheng <Jeff.zheng@keeson.com>
2023-10-26 15:33:52 +0800

    init

---
+++ src/main/java/com/example/databasedesign/DatabaseDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/databasedesign/DatabaseDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
